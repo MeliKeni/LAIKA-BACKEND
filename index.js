a9cd8bbbf3869cd80154751ef1b99b9eea07fee9
Bensagra <bensagra@gmail.com>
2026-05-12 15:38:39 -0300

    Update battery level to 87 in /robot endpoint response

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 /*para conectarse al robot, por ahora fake*/
 app.get("/robot", (req, res) => {
     res.json({
-        battery: 83,
+        battery: 87,
         speed: 1.4,
         status: "Juan"
     });
